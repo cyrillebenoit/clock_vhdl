e9944926639357a1d6b8f65a248bdc9b146525b6
cyrillebenoit <cb@bonnetdejour.fr>
2016-02-24 19:02:40 +0100

    Ajout de la gestion des 4 led - hex1 à hex4 en entrée -> reste à déterminer ces données à partir de heures, minutes. Mapping des entrées/sorties à faire

--- principal.vhd
+++ principal.vhd
@@ -32,17 +32,23 @@ use ieee.std_logic_unsigned.all;
 --use UNISIM.VComponents.all;
 
 entity principal is
-    Port ( clk : in  STD_LOGIC;
-           led : out  STD_LOGIC_VECTOR (6 downto 0);
-           an : out  STD_LOGIC_VECTOR (3 downto 0));
+    Port ( clk1 : in  STD_LOGIC;
+			  led1 : out  STD_LOGIC_VECTOR (6 downto 0);
+			  led2 : out  STD_LOGIC_VECTOR (6 downto 0);
+			  led3 : out  STD_LOGIC_VECTOR (6 downto 0);
+			  led4 : out  STD_LOGIC_VECTOR (6 downto 0);
 end principal;
 
 architecture Behavioral of principal is
-
+	signal seconds,minutes : std_logic_vector (5 downto 0);
+	signal hours : STD_LOGIC_VECTOR (4 downto 0);
 	signal sec,min,hour : integer range 0 to 60 :=0;
 	signal count : integer :=1;
 	signal clk : std_logic :='0';
-	signal hex_out : std_logic_vector (3 downto 0);
+	signal hex1 : std_logic_vector (3 downto 0);
+	signal hex2 : std_logic_vector (3 downto 0);
+	signal hex3 : std_logic_vector (3 downto 0);
+	signal hex4 : std_logic_vector (3 downto 0);
 	
 	begin
 		seconds <= conv_std_logic_vector(sec,6);
@@ -79,7 +85,78 @@ architecture Behavioral of principal is
 				end if;
 			end if;
 		end process;
-		
+
+			 with hex1 SELect
+			led1<= "1111001" when "0001",   --1
+					 "0100100" when "0010",   --2
+					 "0110000" when "0011",   --3
+					 "0011001" when "0100",   --4
+					 "0010010" when "0101",   --5
+					 "0000010" when "0110",   --6
+					 "1111000" when "0111",   --7
+					 "0000000" when "1000",   --8
+					 "0010000" when "1001",   --9
+					 "0001000" when "1010",   --A
+					 "0000011" when "1011",   --b
+					 "1000110" when "1100",   --C
+					 "0100001" when "1101",   --d
+					 "0000110" when "1110",   --E
+					 "0001110" when "1111",   --F
+					 "1000000" when others;   --0
+					 
+				with hex2 SELect
+			led2<= "1111001" when "0001",   --1
+					 "0100100" when "0010",   --2
+					 "0110000" when "0011",   --3
+					 "0011001" when "0100",   --4
+					 "0010010" when "0101",   --5
+					 "0000010" when "0110",   --6
+					 "1111000" when "0111",   --7
+					 "0000000" when "1000",   --8
+					 "0010000" when "1001",   --9
+					 "0001000" when "1010",   --A
+					 "0000011" when "1011",   --b
+					 "1000110" when "1100",   --C
+					 "0100001" when "1101",   --d
+					 "0000110" when "1110",   --E
+					 "0001110" when "1111",   --F
+					 "1000000" when others;   --0
+	 
+				with hex3 SELect
+			led3<= "1111001" when "0001",   --1
+					 "0100100" when "0010",   --2
+					 "0110000" when "0011",   --3
+					 "0011001" when "0100",   --4
+					 "0010010" when "0101",   --5
+					 "0000010" when "0110",   --6
+					 "1111000" when "0111",   --7
+					 "0000000" when "1000",   --8
+					 "0010000" when "1001",   --9
+					 "0001000" when "1010",   --A
+					 "0000011" when "1011",   --b
+					 "1000110" when "1100",   --C
+					 "0100001" when "1101",   --d
+					 "0000110" when "1110",   --E
+					 "0001110" when "1111",   --F
+					 "1000000" when others;   --0
+				
+				with hex4 SELect
+			led4<= "1111001" when "0001",   --1
+					 "0100100" when "0010",   --2
+					 "0110000" when "0011",   --3
+					 "0011001" when "0100",   --4
+					 "0010010" when "0101",   --5
+					 "0000010" when "0110",   --6
+					 "1111000" when "0111",   --7
+					 "0000000" when "1000",   --8
+					 "0010000" when "1001",   --9
+					 "0001000" when "1010",   --A
+					 "0000011" when "1011",   --b
+					 "1000110" when "1100",   --C
+					 "0100001" when "1101",   --d
+					 "0000110" when "1110",   --E
+					 "0001110" when "1111",   --F
+					 "1000000" when others;   --0
 		
 end Behavioral;
 
