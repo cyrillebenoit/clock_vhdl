44636cb4b7d3afc4b61a5fae7469a3117098a090
cyrillebenoit <cb@bonnetdejour.fr>
2016-02-24 19:47:49 +0100

    Ajout des fichiers manquants

---
+++ pa.fromHdl.tcl
@@ -0,0 +1,13 @@
+
+# PlanAhead Launch Script for Pre-Synthesis Floorplanning, created by Project Navigator
+
+create_project -name projetS4 -dir "/home/cyrille/projetS4/clock_vhdl/planAhead_run_1" -part xc3s100ecp132-4
+set_param project.pinAheadLayout yes
+set srcset [get_property srcset [current_run -impl]]
+set_property target_constrs_file "principal.ucf" [current_fileset -constrset]
+set hdlfile [add_files [list {principal.vhd}]]
+set_property file_type VHDL $hdlfile
+set_property library work $hdlfile
+set_property top principal $srcset
+add_files [list {principal.ucf}] -fileset [get_property constrset [current_run]]
+open_rtl_design -part xc3s100ecp132-4
