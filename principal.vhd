014a3170d299ff4bf0aa5595dc632d2840db075e
cyrillebenoit <cb@bonnetdejour.fr>
2016-02-24 14:40:48 +0100

    Inital code - indented correctly

--- principal.vhd
+++ principal.vhd
@@ -18,7 +18,9 @@
 --
 ----------------------------------------------------------------------------------
 library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_arith.all;
+use ieee.std_logic_unsigned.all;
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
@@ -37,11 +39,47 @@ end principal;
 
 architecture Behavioral of principal is
 
-	hex_out : STD_LOGIC_VECTOR (3 downto 0);
-
-begin
-
+	signal sec,min,hour : integer range 0 to 60 :=0;
+	signal count : integer :=1;
+	signal clk : std_logic :='0';
+	signal hex_out : std_logic_vector (3 downto 0);
 	
+	begin
+		seconds <= conv_std_logic_vector(sec,6);
+		minutes <= conv_std_logic_vector(min,6);
+		hours <= conv_std_logic_vector(hour,5);
 
+
+		 --clk generation. Génère une clock de 1Hz à partir d'une clock de 100MHz
+		process(clk1)
+		begin
+				if(clk1'event and clk1='1') then
+					count <=count+1;
+					if(count = 25000000) then
+						clk <= not clk;
+						count <=1;
+					end if;
+				end if;
+		end process;
+
+		process(clk)   --period of clk is 1 second.	
+		begin
+			if(clk'event and clk='1') then
+				sec <= sec+ 1;
+				if(sec = 59) then
+					sec<=0;
+					min <= min + 1;
+					if(min = 59) then
+						hour <= hour + 1;
+						min <= 0;
+						if(hour = 23) then
+							hour <= 0;
+						end if;
+					end if;
+				end if;
+			end if;
+		end process;
+		
+		
 end Behavioral;
 
