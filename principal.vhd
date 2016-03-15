68055c4f50789b1d601365e5c31fb86abe9aad8a
Alain <alain.berrier@outlook.com>
2016-03-15 22:09:47 +0100

    FINAL RELEASE : * Modified and cleaned the code for the countdown + Added "Piezo Test Sound" + Added AM/PM mode * Minor modifications

--- principal.vhd
+++ principal.vhd
@@ -62,26 +62,33 @@ architecture Behavioral of principal is
 	signal value : integer range 0 to 10 :=0;
 	signal cadran : integer range 0 to 4 :=1;  		  -- selectionne le cadran a  utiliser (permet de changer de cadran et de savoir ou on en est)
 	signal count2 : std_logic :='0';	  
-	signal test_led : std_logic_vector (7 downto 0):="00000000";
 	
-	--CHRONOMETRE
-	signal chrono_usec,chrono_umin,chrono_dsec,chrono_dmin: integer range 0 to 10 :=0;
-	signal chrono_usec2,chrono_umin2,chrono_dsec2,chrono_dmin2: integer range 0 to 10 :=0;
-	signal chrono_usec3,chrono_umin3,chrono_dsec3,chrono_dmin3: integer range 0 to 10 :=1;
-	signal chrono_sec_clk : std_logic :='0';
-	signal chrono_min_clk : std_logic :='0';
+	--COMPTE A REBOURS
+	signal countdown_state : std_logic :='0'; -- 0 lorsque le chronomètre est en mode set, 1 lorsqu'il est en décompte
+	signal countdown_done : std_logic:='0'; --'1' lorsque le décompte est terminé -> PULSE
+	
+	signal countdown_usec,countdown_umin,countdown_dsec,countdown_dmin: integer range 0 to 10 :=0;
+	signal countdown_usec_set,countdown_umin_set,countdown_dsec_set,countdown_dmin_set: integer range 0 to 10 :=0;
+	signal countdown_usec_cd,countdown_umin_cd,countdown_dsec_cd,countdown_dmin_cd: integer range 0 to 10 :=1;
+	signal countdown_set_sec_clk : std_logic :='0';
+	signal countdown_set_min_clk : std_logic :='0';
 	signal countdown_sec_clk : std_logic :='0';
 	signal countdown_min_clk : std_logic :='0';
-	signal countdown_hour_clk : std_logic :='0';
-	signal countdown_Minute : std_logic :='0';
-	signal countdown_Hour : std_logic :='0';
+	
+	signal buzzer_end : integer range 0 to 5 :=0;
+	signal buzzer_start : std_logic :='0';
+	signal countdown_permut_state : std_logic :='0'; --PULSE
+	signal buzzer_toggle : std_logic :='0';
+	
+	--Signaux messagers - permet de créer des pulses
+	signal countdown_permut_state_receive : std_logic:='0';
 	signal countdown_toggle_done : std_logic :='0';
-	signal chrono_permut, chrono_permut_receive : std_logic:='0';
-	signal buzzer_start,buzzer_toggle : std_logic :='0';
-	signal buzzer_end : integer range 0 to 4 :=0;
-
-	signal countdown_done : std_logic:='0';
-	signal chrono_toggle : std_logic :='0'; -- 0 lorsque le chronomètre est en mode set, 1 lorsqu'il est en décompte
+	
+	--MODE AM/PM
+	signal AP_uhour,AP_dhour : integer range 0 to 10 :=0;
+	signal AP_led : std_logic :='0';
+	
+	
 	
 begin
 	--FS1
@@ -98,11 +105,11 @@ begin
 	end process;
 	
 	--FS7 : AVSEC
-	sec_clk <= count(22) when switch(0)='0' and switch(1)='0' and (button(0)='1' or button(1)='1')
+	sec_clk <= count(22) when switch(0)='0' and switch(1)='0' and button(0)='1'
 	else clk;
 	
 	--FS2 : Compteur de secondes
-	process(sec_clk)
+	process(sec_clk,button,switch)
 	begin
 		if(sec_clk'event and sec_clk='1') then
 			--Modification des unités
@@ -119,14 +126,19 @@ begin
 				usec<=usec+1;
 			end if;
 		end if;
+		--RESET
+		if(button(1)='1' and switch(0)='0' and switch(1)='0') then
+			usec<=0;
+			dsec<=0;
+		end if;
 	end process;
 	
 	--FS9 : AVMIN
-	min_clk <= count(22) when (switch(0)='0' and switch(1)='0' and (button(2)='1' or button(3)='1')) or (switch(0)='1' and switch(1)='0' and (button(0)='1' or button(1)='1'))
+	min_clk <= count(22) when (switch(0)='0' and switch(1)='0' and button(2)='1') or (switch(0)='1' and switch(1)='0' and button(0)='1')
 	else Minute;
 	
 	--FS3 : Compteur de minutes
-	process(min_clk)
+	process(min_clk,button,switch)
 	begin
 		if(min_clk'event and min_clk='1') then
 			
@@ -144,14 +156,19 @@ begin
 				umin<=umin+1;
 			end if;
 		end if;
+		--RESET
+		if((button(3)='1' and switch(0)='0' and switch(1)='0') or ( button(1)='1' and switch(0)='1' and switch(1)='0')) then
+			umin<=0;
+			dmin<=0;
+		end if;
 	end process;
 	
 	--FS10 : AVHOUR
-	hour_clk <= count(22) when switch(0)='1' and switch(1)='0' and (button(2)='1' or button(3)='1')
+	hour_clk <= count(22) when switch(0)='1' and switch(1)='0' and button(2)='1'
 	else Hour;
 	
 	--FS4 : Compteur des heures
-	process(hour_clk)
+	process(hour_clk,button,switch)
 	begin
 		if(hour_clk'event and hour_clk='1') then
 			--Modification des unités
@@ -164,6 +181,10 @@ begin
 			else
 				uhour<=uhour+1;
 			end if;
+		end if;
+		if(button(3)='1' and switch(0)='1' and switch(1)='0') then
+			uhour<=0;
+			dhour<=0;
 		end if;
 	end process;
 	
@@ -182,26 +203,6 @@ begin
 		end if;
 	end process;
 	
-	--FS5 : Multiplexeur : Choix de la valeur de value à afficher en fonction des differents paramètres
-	
-	-- Mode MM:SS
-	value <= usec when cadran=1 and switch(0)='0' and switch(1)='0' 
-	else dsec when  cadran=2 and switch(0)='0' and switch(1)='0' 
-	else umin when  cadran=3 and switch(0)='0' and switch(1)='0' 
-	else dmin when  cadran=4 and switch(0)='0' and switch(1)='0' 
-
-	-- Mode HH:MM
-	else umin when cadran=1 and switch(0)='1' and switch(1)='0' 
-	else dmin when  cadran=2 and switch(0)='1' and switch(1)='0' 
-	else uhour when  cadran=3 and switch(0)='1' and switch(1)='0' 
-	else dhour when  cadran=4 and switch(0)='1' and switch(1)='0' 
-	
-	-- Mode chrono
-	else chrono_usec when cadran=1 and switch(1)='1' 
-	else chrono_dsec when cadran=2 and switch(1)='1'
-	else chrono_umin when cadran=3 and switch(1)='1'
-	else chrono_dmin when cadran=4 and switch(1)='1';
-	
 	-- Convertisseur 7 segments : traduit la valeur de value en segments à afficher	
 	led<="1000000"	when value=0
 	else "1111001" when value=1
@@ -218,200 +219,223 @@ begin
 	
 	--FS6 : Démultiplexeur : Choix du cadran
 	--Mode chrono set
-	an <="1111" when chrono_toggle='0' and clk='0' and switch(1)='1'
+	an <="1111" when countdown_state='0' and clk='0' and switch(1)='1'
 	--Mode normal et chrono décompte
 	else "1110" when cadran=1
 	else "1101" when cadran=2
 	else "1011" when cadran=3
 	else "0111" when cadran=4;
-			
-	--BONUS
-	-- Chronomètre
-	-- Alarme
-	-- Mode am/pm
 	
-	--FSB1 Test
-	process(usec)
-	begin
-		-- TEST
-		bin <= switch;
-	end process;
+	--FS5 : Multiplexeur : Choix de la valeur de value à afficher en fonction des differents paramètres
 	
-	--Chronomètre
+	-- Mode MM:SS
+	value <= usec when cadran=1 and switch(0)='0' and switch(1)='0' 
+	else dsec when  cadran=2 and switch(0)='0' and switch(1)='0' 
+	else umin when  cadran=3 and switch(0)='0' and switch(1)='0' 
+	else dmin when  cadran=4 and switch(0)='0' and switch(1)='0' 
+
+	-- Mode HH:MM
+	else umin when cadran=1 and switch(0)='1' and switch(1)='0' 
+	else dmin when  cadran=2 and switch(0)='1' and switch(1)='0' 
+	else uhour when  cadran=3 and switch(0)='1' and switch(1)='0' and switch(3)='0'
+	else dhour when  cadran=4 and switch(0)='1' and switch(1)='0' and switch(3)='0'
 	
-	--FSB1
-	chrono_usec <= 0 when switch(1)='1' and button(3)='1' --RESET
-	else chrono_usec2 when chrono_toggle='0' --SET
-	else chrono_usec3 when chrono_toggle='1'; --DECOMPTE
+	--Mode AM/PM
+	else AP_uhour when  cadran=3 and switch(0)='1' and switch(1)='0' and switch(3)='1'
+	else AP_dhour when  cadran=4 and switch(0)='1' and switch(1)='0' and switch(3)='1'
 	
-	chrono_dsec <= 0 when switch(1)='1' and button(3)='1' --RESET
-	else chrono_dsec2 when chrono_toggle='0' --SET
-	else chrono_dsec3 when chrono_toggle='1'; --DECOMPTE
+	-- Mode compte à rebours
+	else countdown_usec when cadran=1 and switch(1)='1' 
+	else countdown_dsec when cadran=2 and switch(1)='1'
+	else countdown_umin when cadran=3 and switch(1)='1'
+	else countdown_dmin when cadran=4 and switch(1)='1';
 	
-	chrono_umin <= 0 when switch(1)='1' and button(3)='1' --RESET
-	else chrono_umin2 when chrono_toggle='0' --SET
-	else chrono_umin3 when chrono_toggle='1'; --DECOMPTE
+
+	--FSB1a :  Affichage LED
+	bin <= "11111111" when AP_led='1' and switch(3)='1' and switch(1)='0'
+	else "00000000" when AP_led='0' and switch(3)='1' and switch(1)='0'
+	else switch;
 	
-	chrono_dmin <= 0 when switch(1)='1' and button(3)='1' --RESET
-	else chrono_dmin2 when chrono_toggle='0' --SET
-	else chrono_dmin3 when chrono_toggle='1'; --DECOMPTE
+	--COMPTE A REBOURS
+	
+	--FSB2 : Multiplexeur - Assignation des valeurs à afficher selon le mode du compte à rebours
+	countdown_usec <= 0 when switch(1)='1' and button(3)='1' --RESET
+	else countdown_usec_set when countdown_state='0' --SET
+	else countdown_usec_cd when countdown_state='1'; --DECOMPTE
+	
+	countdown_dsec <= 0 when switch(1)='1' and button(3)='1' --RESET
+	else countdown_dsec_set when countdown_state='0' --SET
+	else countdown_dsec_cd when countdown_state='1'; --DECOMPTE
+	
+	countdown_umin <= 0 when switch(1)='1' and button(3)='1' --RESET
+	else countdown_umin_set when countdown_state='0' --SET
+	else countdown_umin_cd when countdown_state='1'; --DECOMPTE
+	
+	countdown_dmin <= 0 when switch(1)='1' and button(3)='1' --RESET
+	else countdown_dmin_set when countdown_state='0' --SET
+	else countdown_dmin_cd when countdown_state='1'; --DECOMPTE
 	
 	
-	--FSB2 : AVSEC CHRONO
-	chrono_sec_clk <= count(22) when switch(1)='1' and button(0)='1' and chrono_toggle='0'
+	--FSB3a : AVSEC CHRONO
+	countdown_set_sec_clk <= count(22) when switch(1)='1' and button(0)='1' and countdown_state='0'
 	else '0';
 	
-	--FSB3 : Compteur de secondes du chrono
-	process(chrono_sec_clk)
+	--FSB3b : Compteur de secondes du chrono
+	process(countdown_set_sec_clk)
 	begin
-		if(chrono_sec_clk'event and chrono_sec_clk='1') then
-			--Modification des unités
-			if(chrono_usec2=9) then
-				chrono_usec2<=0;
-				if(chrono_dsec2=5) then
-					chrono_dsec2<=0;
+		if(countdown_set_sec_clk'event and countdown_set_sec_clk='1') then
+			if(countdown_usec_set=9) then
+				countdown_usec_set<=0;
+				if(countdown_dsec_set=5) then
+					countdown_dsec_set<=0;
 				else
-					chrono_dsec2<=chrono_dsec2+1;
+					countdown_dsec_set<=countdown_dsec_set+1;
 				end if;
 			else
-				chrono_usec2<=chrono_usec2+1;
+				countdown_usec_set<=countdown_usec_set+1;
 			end if;
 		end if;
+		--RESET
 		if(button(3)='1') then
-			chrono_usec2<=0;
-			chrono_dsec2<=0;
+			countdown_usec_set<=0;
+			countdown_dsec_set<=0;
 		end if;
 	end process;
 	
-	--FSB2 : AVMIN CHRONO
-	chrono_min_clk <= count(22) when switch(1)='1' and button(1)='1' and chrono_toggle='0'
+	--FSB4a : AVMIN CHRONO
+	countdown_set_min_clk <= count(22) when switch(1)='1' and button(1)='1' and countdown_state='0'
 	else '0';
 	
-	--FSB3 : Compteur de minutes du chrono
-	process(chrono_min_clk)
+	--FSB4b : Compteur de minutes du chrono
+	process(countdown_set_min_clk)
 	begin
-		if(chrono_min_clk'event and chrono_min_clk='1') then
-			--Modification des unités
-			if(chrono_umin2=9) then
-				chrono_umin2<=0;
-				if(chrono_dmin2=5) then
-					chrono_dmin2<=0;
+		if(countdown_set_min_clk'event and countdown_set_min_clk='1') then
+			if(countdown_umin_set=9) then
+				countdown_umin_set<=0;
+				if(countdown_dmin_set=5) then
+					countdown_dmin_set<=0;
 				else
-					chrono_dmin2<=chrono_dmin2+1;
+					countdown_dmin_set<=countdown_dmin_set+1;
 				end if;
 			else
-				chrono_umin2<=chrono_umin2+1;
+				countdown_umin_set<=countdown_umin_set+1;
 			end if;
 		end if;
+		--RESET
 		if(button(3)='1') then
-			chrono_umin2<=0;
-			chrono_dmin2<=0;
+			countdown_umin_set<=0;
+			countdown_dmin_set<=0;
 		end if;
 	end process;
 	
-	--Variable d'incrémentation des valeurs lorsque basculement en decompte
-	process(chrono_toggle)
+	--FSB5 : Pulseur d'activation de la permutation des valeurs
+	process(countdown_state)
 	begin
-		if(chrono_toggle'event and chrono_toggle='1') then
-			chrono_permut<='1';
+		if(countdown_state'event and countdown_state='1') then --Lorsque front montant de l'état du compteur
+			countdown_permut_state<='1'; --Activation de la permutation
 		end if;
-		if(chrono_permut_receive='1') then
-			chrono_permut<='0';
+		if(countdown_permut_state_receive='1') then --Lorsque message de confirmation envoyé
+			countdown_permut_state<='0'; --Desactivation de la permutation
 		end if;
 	end process;
 	
-	--FSB6 : Changement d'état du chrono
+	--FSB6 : Changement d'état du compte à rebours
 	
-	process(chrono_toggle,button,switch,countdown_done)
+	process(countdown_state,button,switch,countdown_done)
 	begin
 		if(button(2)='1' and switch(1)='1') then
-			chrono_toggle<='1';
-		elsif((button(3)='1' and switch(1)='1') or countdown_done='1') then
-			chrono_toggle<='0';
+			countdown_state<='1';
+		elsif((button(3)='1' and switch(1)='1') or countdown_done='1' or switch(1)='0') then
+			countdown_state<='0';
 			countdown_toggle_done<='1';
-			--AJOUTER PIEZO
 		else
 			countdown_toggle_done<='0';
 		end if;
 	end process;
 	
-	--FSB7 : AVSEC DECOMPT
-	countdown_sec_clk <= clk when chrono_toggle<='1'
+	--FSB7a : AVSEC DECOMPT
+	countdown_sec_clk <= clk when countdown_state<='1'
 	else '0';
 	
 	
-	--FSB8 : Décompteur de secondes du chrono
-	process(countdown_sec_clk,chrono_permut)
+	--FSB7b : Décompteur de secondes du chrono
+	process(countdown_sec_clk,countdown_permut_state,button,switch)
 	begin
 		if(countdown_sec_clk'event and countdown_sec_clk='1') then
-			if(chrono_usec3=0) then
-				chrono_usec3<=9;
-				if(chrono_dsec3=0) then
-					countdown_Minute <='1';
-					chrono_dsec3<=5;
+			if(countdown_usec_cd=0) then
+				countdown_usec_cd<=9;
+				if(countdown_dsec_cd=0) then
+					countdown_min_clk <='1';
+					countdown_dsec_cd<=5;
 				else
-					chrono_dsec3<=chrono_dsec3-1;
-					countdown_Minute <='0';
+					countdown_dsec_cd<=countdown_dsec_cd-1;
+					countdown_min_clk <='0';
 				end if;
 			else
-				countdown_Minute <='0';
-				chrono_usec3<=chrono_usec3-1;
+				countdown_min_clk <='0';
+				countdown_usec_cd<=countdown_usec_cd-1;
 			end if;
 		end if;
-		if(chrono_permut='1') then
-			chrono_permut_receive<='1';
-			chrono_usec3<=chrono_usec2;
-			chrono_dsec3<=chrono_dsec2;
+		--Permutation des valeurs SET/COUNTDOWN
+		if(countdown_permut_state='1') then
+			countdown_permut_state_receive<='1';
+			countdown_usec_cd<=countdown_usec_set;
+			countdown_dsec_cd<=countdown_dsec_set;
 		else
-			chrono_permut_receive<='0';
+			countdown_permut_state_receive<='0';
 		end if;
-		if(button(3)='1') then
-			chrono_usec3<=0;
-			chrono_dsec3<=0;
+		---RESET
+		if(button(3)='1' and switch(1)='1') then
+			countdown_usec_cd<=0;
+			countdown_dsec_cd<=0;
 		end if;
 	end process;
 	
-	--FSB5 : Décompteur de minutes du chrono
-	process(countdown_Minute,chrono_permut,countdown_toggle_done)
+	--FSB8 : Décompteur de minutes du chrono
+	process(countdown_min_clk,countdown_permut_state,countdown_toggle_done,button,switch)
 	begin
-		if(countdown_Minute'event and countdown_Minute='1') then
-			if(chrono_umin3=0) then
-				chrono_umin3<=9;
-				if(chrono_dmin3=0) then
-					chrono_dmin3<=0;
-					chrono_umin3<=0;
-					countdown_done<='1';
+		if(countdown_min_clk'event and countdown_min_clk='1') then
+			if(countdown_umin_cd=0) then
+				countdown_umin_cd<=9;
+				if(countdown_dmin_cd=0) then
+					countdown_dmin_cd<=0;
+					countdown_umin_cd<=0;
+					countdown_done<='1'; --Envoie de la pulse que le compte à rebours a terminé.
 				else
-					chrono_dmin3<=chrono_dmin3-1;
+					countdown_dmin_cd<=countdown_dmin_cd-1;
 				end if;
 			else
-				chrono_umin3<=chrono_umin3-1;
+				countdown_umin_cd<=countdown_umin_cd-1;
 			end if;
 		end if;
-		if(chrono_permut='1') then
-			chrono_permut_receive<='1';
-			chrono_umin3<=chrono_umin2;
-			chrono_dmin3<=chrono_dmin2;
+		--Permutation des valeurs SET/COUNTDOWN
+		if(countdown_permut_state='1') then
+			countdown_permut_state_receive<='1';
+			countdown_umin_cd<=countdown_umin_set;
+			countdown_dmin_cd<=countdown_dmin_set;
 		else
-			chrono_permut_receive<='0';
+			countdown_permut_state_receive<='0';
 		end if;
 		if(countdown_toggle_done='1') then
 			countdown_done<='0';
 		end if;
-		if(button(3)='1') then
-			chrono_umin3<=0;
-			chrono_dmin3<=0;
+		--RESET
+		if(button(3)='1' and switch(1)='1') then
+			countdown_umin_cd<=0;
+			countdown_dmin_cd<=0;
 		end if;
 	end process;
 	
-	--Activation et desactivation du buzzer
-	piezo <= clk when countdown_done='1'
+	-- FSB9 & FSB1b : Activation et desactivation du buzzer
+	
+	buzzer_start <= '1' when countdown_done='1' and button(3)='0' --Activation sur pulse non provoquée par le RESET
+	else '0' when buzzer_end=0; --Desactivation à la fin du compteur
+	
+	piezo <= clk when buzzer_start='1'
+	else count(22) when switch(2)='1' --FSB1b : Activation du buzzer sur levier
 	else '0' when buzzer_end =0;
 	
-	--Compteur du buzzer
-	buzzer_start <= '1' when countdown_done='1'
-	else '0' when buzzer_end=0;
-	
+	--Repeater
 	process(clk,buzzer_start, buzzer_end)
 	begin
 		if(clk'event and clk='1') then
@@ -419,13 +443,40 @@ begin
 				buzzer_end<=buzzer_end-1;
 			end if;
 		end if;
+		--Activation du décompte
 		if(buzzer_start='1' and buzzer_toggle='0') then
 				buzzer_toggle<='1';
 				buzzer_end<=4;
 		end if;
+		--Remise à l'état initial
 		if(buzzer_end=0) then
 		 buzzer_toggle<='0';
 		end if;
 	end process;
 	
+	--FSB10 : Mode AM/PM
+	process(uhour,dhour,switch)
+	begin
+		if((uhour >= 2 and dhour =1) or dhour=2) then --mode PM
+			AP_led<='1';
+			--Changement de AP_dhour et AP_uhour : 4 états possibles
+			if(dhour=1 and uhour<=9) then
+				AP_dhour<=0;
+				AP_uhour<=uhour-2;
+			elsif(dhour=2 and uhour<=1) then
+				AP_dhour<=0;
+				AP_uhour<=uhour+8;
+			elsif(dhour=2 and uhour<=3) then
+				AP_dhour<=1;
+				AP_uhour<=uhour-2;
+			elsif(dhour=2 and uhour=4) then
+				AP_dhour<=0;
+				AP_uhour<=0;
+			end if;
+		else
+			AP_led<='0';
+			AP_dhour<=dhour;
+			AP_uhour<=uhour;
+		end if;
+	end process;
 end Behavioral;
